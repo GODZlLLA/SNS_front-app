269c7151c574c431b83e5d5bdb09e7875dc51928
GODZlLLA <adacchann613@gmail.com>
2025-02-03 20:04:00 +0900

    📁 add(#1): サンプルボタン追加

--- my-sns-project/src/screen/App.tsx
+++ my-sns-project/src/screen/App.tsx
@@ -1,12 +1,14 @@
+import { Button } from '@/components/button';
 import { registerRootComponent } from 'expo';
 import { StatusBar as ExpoStatusBar } from 'expo-status-bar';
-import { Button, StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View } from 'react-native';
 
 const App = () => {
   return (
     <View style={styles.container}>
       <Text>Open up App.tsx to start working on your app!</Text>
-      <Button title='Click me' />
+      <Button label='写真を選択' theme='primary' />
+      <Button label='この写真を使用' />
       <ExpoStatusBar style='auto' />
     </View>
   );
