32a1fc79e392bad1313d7d20bf386223f13836e4
GODZlLLA <adacchann613@gmail.com>
2025-02-03 20:11:08 +0900

    🛠️ update(#1): 画面の横幅、縦幅取得

--- my-sns-project/src/screen/App.tsx
+++ my-sns-project/src/screen/App.tsx
@@ -1,4 +1,5 @@
 import { Button } from '@/components/button';
+import { SCREEN_HEIGHT, SCREEN_WIDTH } from '@/constants';
 import { registerRootComponent } from 'expo';
 import { StatusBar as ExpoStatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
@@ -7,6 +8,7 @@ const App = () => {
   return (
     <View style={styles.container}>
       <Text>Open up App.tsx to start working on your app!</Text>
+      <Text>Screen Width: {SCREEN_WIDTH} & Height: {SCREEN_HEIGHT}</Text>
       <Button label='写真を選択' theme='primary' />
       <Button label='この写真を使用' />
       <ExpoStatusBar style='auto' />
